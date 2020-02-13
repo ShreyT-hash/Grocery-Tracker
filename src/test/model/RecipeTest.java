43386a34fca7915db49435ca71cd3b716feb1e8c
Shrey <shrey.thapar.uni@gmail.com>
2020-02-12 18:10:23 -0800

    Finished all user stories and tests, added proper documentation to everything and fixed checkstyle errors pt. 3

--- src/test/model/RecipeTest.java
+++ src/test/model/RecipeTest.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RecipeTest {
+
     ArrayList <GroceryItem> penneArabiatta = new ArrayList<>();
     ArrayList <GroceryItem> cereal = new ArrayList<>();
     Inventory inventory = new Inventory();
@@ -15,7 +16,7 @@ public class RecipeTest {
     GroceryItem milk = new GroceryItem("milk", 2);
     GroceryItem cheetoes = new GroceryItem("cheetoes", 1);
     GroceryItem sugar = new GroceryItem("sugar", 1.4);
-    GroceryItem cheerioes = new GroceryItem("cheeroes", 1.8);
+    GroceryItem cheerioes = new GroceryItem("cheerioes", 1.8);
 
     GroceryItem tomatoSauce = new GroceryItem("tomato sauce", 3.2);
     GroceryItem pasta = new GroceryItem("pasta", 1.4);
@@ -43,4 +44,57 @@ public class RecipeTest {
         assertTrue(recipes.containsRecipe(inventory.getToCookItems(), cereal));
 
     }
+
+    @Test
+    void testConstructor(){
+        Inventory inventory = new Inventory();
+        assertEquals(inventory.getFridgeItems().size(), 0);
+        assertEquals(inventory.getToCookItems().size(), 0);
+    }
+    @Test
+    void testAddItem(){
+        assertEquals(inventory.getFridgeItems().size(), 0);
+        inventory.addItemToInventory(milk, 2, true);
+        assertEquals(inventory.getFridgeItems().size(), 1);
+    }
+
+    @Test
+    void testSum(){
+        inventory.addItemToInventory(milk, 2, true);
+        inventory.addItemToInventory(cheetoes, 4, false);
+        inventory.addItemToInventory(sugar, 1, false);
+        assertEquals(inventory.totalPrice(), 4.4);
+    }
+
+    @Test
+    void testSumToString(){
+        inventory.addItemToInventory(milk, 1, true);
+        inventory.addItemToInventory(cheetoes, 4, false);
+        inventory.addItemToInventory(sugar, 2, false);
+        assertEquals(inventory.sumToString(), "You spent $4.4 on groceries this time");
+    }
+
+
+    @Test
+    void testInventoryDisplay(){
+        inventory.addItemToInventory(milk, 1, true);
+        inventory.addItemToInventory(cheetoes, 2, false);
+        inventory.addItemToInventory(cheerioes, 3, true);
+        assertEquals(inventory.viewInventory(), "Item: milk/ Quantity: 1.0/ Perishable: true\n" +
+                "Item: cheetoes/ Quantity: 2.0/ Perishable: false\n" +
+                "Item: cheerioes/ Quantity: 3.0/ Perishable: true\n");
+
+    }
+
+    @Test
+    void testBoolCount(){
+        inventory.addItemToInventory(milk, 1, true);
+        assertEquals(inventory.perishableCount(), 1);
+        inventory.addItemToInventory(cheetoes, 2, false);
+        assertEquals(inventory.perishableCount(), 1);
+        inventory.addItemToInventory(cheerioes, 3, true);
+        assertEquals(inventory.perishableCount(), 2);
+
+    }
+
 }
